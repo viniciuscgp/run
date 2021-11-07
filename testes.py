703fb04de0ce58d5b727a5e7657858b6176c769b
viniciuscgp <vinians2006@yahoo.com.br>
2021-11-07 16:06:13 -0300

    Added .gitignore Removed testes.py

--- testes.py
+++ testes.py
@@ -1,4 +1,9 @@
-str = "Vinicius"
-print(str)
-str.upper()
-print(str)
+print("Criando arquivo:")
+f = open("nomes.txt", "r")
+for n in range(1, 5):
+    linha = f.readline().strip()
+    cols = linha.split("|")
+    print("Nome:{} Valor:{}".format(cols[0], cols[1]))
+
+f.close()
+print("Finalizado!!!")
